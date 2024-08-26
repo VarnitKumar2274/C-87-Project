2681c06985c95557c222f156edf4ceac106a6a2d
VarnitKumar2274 <kumarvarnit1@gmail.com>
2024-08-26 17:10:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+
+ctx.beginPath()
+ctx.strokestyle= "grey";
+ctx.linewidth= 1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath()
+ctx.strokestyle = "blue";
+ctx.linewidth = 5;
+ctx.arc(250, 210, 40, 0, 2 * MATH.PI);
+ctx.stroke();
+
+ctx.beginPath()
+ctx.strokestyle = "black";
+ctx.linewidth = 5;
+ctx.arc(350, 210, 40, 0, 2 * MATH.PI);
+ctx.stroke();
+
+ctx.beginPath()
+ctx.strokestyle = "red";
+ctx.linewidth = 5;
+ctx.arc(450, 210, 40, 0, 2 * MATH.PI);
+ctx.stroke();
+
+ctx.beginPath()
+ctx.strokestyle = "orange";
+ctx.linewidth = 5;
+ctx.arc(300, 250, 40, 0, 2 * MATH.PI);
+ctx.stroke();
+
+ctx.beginPath()
+ctx.strokestyle = "green";
+ctx.linewidth = 5;
+ctx.arc(400, 250, 40, 0, 2 * MATH.PI);
+ctx.stroke();
